7ead26448a34f92dce88fbb60ee189fd5a0f4c87
Michael Knopke <knopkem@gmail.com>
2023-07-18 22:48:19 +0200

    more

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 var new_direction = Vector2(0, 1)
 var animation
 
+var is_attacking = false
+
 func _physics_process(delta):
 
 	# Get player input (left, right, up/down)
@@ -15,13 +17,28 @@ func _physics_process(delta):
 	# If input is digital, normalize it for diagonal movement
 	if abs(direction.x) == 1 and abs(direction.y) == 1:
 		direction = direction.normalized()
+		
+	#sprinting 
+	if Input.is_action_pressed("ui_sprint"):
+		speed = 100
+	elif Input.is_action_just_released("ui_sprint"):
+		speed = 50
+		
 	# Apply movement
 	var movement = speed * direction * delta
-	# moves our player around, whilst enforcing collisions so that they come to a stop 
-	# when colliding with another object.
-	move_and_collide(movement)
 	#plays animations
-	player_animations(direction)
+	if !is_attacking:
+		player_animations(direction)
+		move_and_collide(movement)
+		
+	if is_attacking and !$AnimatedSprite2D.is_playing():
+		is_attacking = false
+		
+func _input(event):
+	if event.is_action_pressed("ui_attack"):
+		is_attacking = true
+		var animation = "attack_" + returned_direction(new_direction)
+		$AnimatedSprite2D.play(animation)
 
 func player_animations(direction: Vector2):
 	#Vector2.ZERO is the shorthand for writing Vector2(0, 0).
@@ -54,3 +71,8 @@ func returned_direction(direction : Vector2):
 		return "side"
 	#default value emptz
 	return ""
+
+
+func _on_animated_sprite_2d_animation_finished():
+	is_attacking = false
+	
